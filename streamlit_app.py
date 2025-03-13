fe9aa3b26621fa795120604f031e9e3d527c0e0f
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-13 08:27:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,17 @@ elif menu == "Indeks UV":
     ))
     fig.update_layout(margin=dict(t=30, b=30, l=30, r=30))
     st.plotly_chart(fig, use_container_width=True)
-    
+    st.markdown(f"""
+    <div style="text-align: center;"><span style="display: inline-block; padding: 5px 15px; border-radius: 5px;
+                    background-color: {'#d4edda' if last_index <= 2 else '#fcfac0' if last_index <= 5 else '#ffc78f' if last_index <= 7 else '#ff8a8a' if last_index <= 10 else '#e7cafc'};">
+            {"<p style='color: #00ff00;'><strong>✅ Tingkat aman:</strong> Gunakan pelembab tabir surya SPF 30+ dan kacamata hitam.</p>" if last_index <= 2 else
+             "<p style='color: #ffcc00;'><strong>⚠️ Tingkat bahaya sedang:</strong> Oleskan cairan pelembab tabir surya SPF 30+ setiap 2 jam, kenakan pakaian pelindung matahari.</p>" if last_index <= 5 else
+             "<p style='color: #ff6600;'><strong>⚠️ Tingkat bahaya tinggi:</strong> Kurangi paparan matahari antara pukul 10 pagi hingga pukul 4 sore.</p>" if last_index <= 7 else
+             "<p style='color: #ff0000;'><strong>⚠️ Tingkat bahaya sangat tinggi:</strong> Tetap di tempat teduh dan oleskan sunscreen setiap 2 jam.</p>" if last_index <= 10 else
+             "<p style='color: #9900cc;'><strong>❗ Tingkat bahaya ekstrem:</strong> Diperlukan semua tindakan pencegahan karena kulit dan mata dapat rusak dalam hitungan menit.</p>"}
+       </span>
+    </div>
+    """, unsafe_allow_html=True,)
     st.markdown("""
     <div style="text-align: center; font-size: medium; margin-top: 10px; margin-bottom: 40px;">
         <p><b>Pukul:</b> 13:00 </p>
