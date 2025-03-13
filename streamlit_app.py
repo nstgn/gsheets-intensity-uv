c3e763a81a3ee4a0aeff47481277f2691d3684aa
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-13 07:24:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
-# Custom Header
-import streamlit as st
-import plotly.graph_objects as go
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import plotly.graph_objects as go
 
 st.markdown( """
     <style>
